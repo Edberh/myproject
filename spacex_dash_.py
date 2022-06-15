bd146a8c9fca8a221c0a5eed215493ad1b3fdde4
Eduard Hrebeniukov <103170150+Edberh@users.noreply.github.com>
2022-06-15 13:04:55 +0200

    Update spacex_dash_.py

--- spacex_dash_.py
+++ spacex_dash_.py
@@ -1,7 +1,13 @@
-# Libraries
-from dash import Dash, html, dcc
+# Import required libraries
 import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+import plotly.graph_objects as go
 import plotly.express as px
+from dash import no_update
+
 
 # Read the airline data into pandas dataframe
 spacex_df = pd.read_csv("spacex_launch_dash.csv")
